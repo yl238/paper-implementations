14b0cbda8e0deb9a30aa6779feda22cb43c3c09d
Sue Liu <sue.liu@kingfisher.com>
2025-05-18 17:21:10 +0100

    Final version of LSTM for forecasting

--- PyTorch-Forecasting/LSTM.py
+++ PyTorch-Forecasting/LSTM.py
@@ -1,17 +1,18 @@
 import torch.nn as nn
 
+
 class LSTMModel(nn.Module):
     """A simple LSTM-based regression model for time series forecasting.
 
-    This model consists of an LSTM layer followed by a linear layer. 
-    It takes a sequence of time steps as input and predicts the next value 
+    This model consists of an LSTM layer followed by a linear layer.
+    It takes a sequence of time steps as input and predicts the next value
     in the sequence.
 
     Args:
         input_size (int): Number of features in the input at each time step.
         hidden_size (int): Number of features in the hidden state of the LSTM.
         num_layers (int): Number of recurrent layers in the LSTM.
-        dropout (float, optional): Dropout probability for the LSTM layers 
+        dropout (float, optional): Dropout probability for the LSTM layers
             (default: 0.2).
 
     Forward Input:
@@ -21,13 +22,16 @@ class LSTMModel(nn.Module):
         out (torch.Tensor): Output tensor of shape (batch_size, 1),
             the predicted next value for each input sequence.
     """
+
     def __init__(self, input_size, hidden_size, num_layers, dropout=0.2):
         super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(input_size=input_size,
-                            hidden_size=hidden_size,
-                            num_layers=num_layers,
-                            batch_first=True,
-                            dropout=dropout)
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout,
+        )
         self.linear = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
--- PyTorch-Forecasting/train.py
+++ PyTorch-Forecasting/train.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -68,25 +69,31 @@ def train_and_evaluate(
 
 
 if __name__ == "__main__":
-    data = get_apple_stock_data()
+    df = get_apple_stock_data()
     dataset_train, dataset_test = generate_train_and_test(
-        data,
+        df.values,
         train_test_split=0.8,
     )
+    train_size = len(dataset_train)
     # Min-max scaling
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_train = scaler.fit_transform(dataset_train)
     scaled_test = scaler.transform(dataset_test)
 
-    X_train, y_train = create_sequential_dataset(scaled_train)
-    X_test, y_test = create_sequential_dataset(scaled_test)
+    sequence_length = 5
+    X_train, y_train = create_sequential_dataset(
+        scaled_train, sequence_length=sequence_length
+    )
+    X_test, y_test = create_sequential_dataset(
+        scaled_test, sequence_length=sequence_length
+    )
 
     # Set LSTM parameters
     input_size = 1
-    num_layers = 3
-    hidden_size = 128
+    num_layers = 1
+    hidden_size = 50
     output_size = 1
-    dropout = 0.2
+    dropout = 0
 
     model = LSTMModel(
         input_size=input_size,
@@ -95,9 +102,10 @@ if __name__ == "__main__":
         dropout=dropout,
     )
     loss_fn = nn.MSELoss(reduction="mean")
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)  # Learning rate
+    optimizer = optim.Adam(model.parameters(), lr=5.0e-4)  # Learning rate
 
-    batch_size = 32
+    batch_size = 8
+    num_epochs = 1000
 
     train_dataset = TensorDataset(X_train, y_train)
     test_dataset = TensorDataset(X_test, y_test)
@@ -110,5 +118,17 @@ if __name__ == "__main__":
         loss_fn=loss_fn,
         train_loader=train_loader,
         test_loader=test_loader,
-        num_epochs=100,
+        num_epochs=num_epochs,
     )
+
+    with torch.no_grad():
+        y_train_pred = model(X_train).cpu().numpy()
+        y_test_pred = model(X_test).cpu().numpy()
+    y_train_pred = scaler.inverse_transform(y_train_pred)
+    y_test_pred = scaler.inverse_transform(y_test_pred)
+
+    df["train_pred"] = np.nan
+    df["test_pred"] = np.nan
+
+    df.loc[df.index[sequence_length:train_size], 'train_pred'] = y_train_pred
+    df.loc[df.index[train_size+sequence_length:], 'test_pred'] = y_test_pred
